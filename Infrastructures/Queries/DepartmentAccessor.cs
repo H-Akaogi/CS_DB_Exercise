002dcea7de4cd13182745be60ad3a8c29b19b741
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-12 15:49:05 +0900

    updated CS_DB_Exercise

---
+++ Infrastructures/Queries/DepartmentAccessor.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CS_DB_Exercise.Infrastructures.Contexts;
+using CS_DB_Exercise.Infrastructures.Entities;
+
+namespace CS_DB_Sample.Infrastructures.Queries;
+
+public class DepartmentAccessor
+{
+    private readonly AppDbContext _context;
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="context">アプリケーション用DbContext</param>
+    public DepartmentAccessor(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// すべての部署を取得する
+    /// </summary>
+    public List<DepartmentEntity> FindAll()
+    {
+        // ToList()メソッドを使用して、すべての部署を取得する
+        var departments = _context.Departments.ToList();
+        return departments;
+    }
+    /// <summary>
+    /// 指定した部署Idの部署を取得する
+    /// </summary>
+    /// <param name="departmentId">部署Id(主キー)</param>
+    public DepartmentEntity? FindById(int departmentId)
+    {
+        // Find()メソッドを使用して、指定した部署Idの部署を取得する
+        var department = _context.Departments.Find(departmentId);
+        return department;
+    }
+}
